5837e35c57ed7147a0f9520e7ec46f6547a83b07
HS Coetzee <hs.coetzee@gmail.com>
2018-05-10 21:27:36 +0200

    Closes #1

--- main.sh
+++ main.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 EXE=/go/bin/java-buildpack-memory-calculator
 JAVA=java
+EXTRA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1"
 LOADEDCLASSES=$((400*$1))
 STACKTHREADS=$((15 + $1 * 6 / 10))
 
 FULL_CMD="$EXE -loadedClasses $LOADEDCLASSES -poolType metaspace -stackThreads $STACKTHREADS -totMemory $2"
 echo "Full CMD: $FULL_CMD"
 echo
-# OPTS=`$EXE -loadedClasses $LOADEDCLASSES -poolType metaspace -stackThreads $STACKTHREADS -totMemory $2`
-# OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 $OPTS"
-# echo $OPTS
 
 unset t_std t_err
 eval "$( ($FULL_CMD) 2> >(readarray -t t_err; typeset -p t_err) > >(readarray -t t_std; typeset -p t_std) )"
@@ -20,6 +18,6 @@ if [ -n "$t_err" ]; then
 fi
 
 if [ -n "$t_std" ]; then
-    echo "-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 $t_std"
+    echo "$EXTRA_OPTS $t_std"
     exit 0
 fi
